8bf53c2c73eab2746318f83ab69295743390ff7d
Israel David Tarazona Machado <israel.tarazona@PRAGMA.COM.CO>
2019-01-02 09:57:03 -0500

    hash and apiKey

--- src/providers/comics-service/comics-service.ts
+++ src/providers/comics-service/comics-service.ts
@@ -12,10 +12,13 @@ export class ComicsServiceProvider {
 
   constructor(
     private http: HttpClient
-    ) {}
+  ) { }
 
-  getComics(){
-    return this.http.get('https://gateway.marvel.com/v1/public/comics?ts=1&apikey=b5dd158dd0e856443db7fb726fbc6bc9&hash=80182fcb24c6426319114b9e34eafed6');
+  getComics() {
+    const hash_m = '80182fcb24c6426319114b9e34eafed6';
+    const api_key = 'b5dd158dd0e856443db7fb726fbc6bc9';
+
+    return this.http.get('https://gateway.marvel.com/v1/public/comics?ts=1&apikey' + api_key + '=&hash=' + hash_m);
   }
 
 }
